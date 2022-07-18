e1c87b30cee9c1925eb80757c8d209ce0bdf1032
jdosnowflakeuni <109486718+jdosnowflakeuni@users.noreply.github.com>
2022-07-18 06:39:09 -0700

    Update streamlit_app.py
    Add insert statement to snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,6 @@ streamlit.text("The fruit load list contains")
 streamlit.text(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
